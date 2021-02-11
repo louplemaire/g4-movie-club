f0231e40911069f14c2efd3e288bc31b21c17907
louplemaire <44206840+louplemaire@users.noreply.github.com>
2021-02-11 16:33:28 +0100

    :sparkles: Add GenreItem

--- src/components/GenreItem.js
+++ src/components/GenreItem.js
@@ -1,19 +1,43 @@
 import React from 'react'
-import { StyleSheet, Text, TouchableOpacity } from 'react-native'
+import {StyleSheet, View, Text, TouchableOpacity} from 'react-native'
 
 export default class GenreItem extends React.Component {
     render() {
         const genre = this.props.genre
+// export const GenreItem = (props) => {
+//     const {genre, goToType, index} = props;
+
         return (
-            <TouchableOpacity style={styles.genre_container}>
-                <Text>{genre.name}</Text>
+            <TouchableOpacity style={styles.genre_container}> {/* onPress={goToType} */}
+                <View style={styles.button_container}>
+                    <Text style={styles.button_text}>{genre.name}</Text>
+                </View>
             </TouchableOpacity>
         )
     }
 }
 
 const styles = StyleSheet.create({
-    genre_container: {
-        height: 50
-    }
+    button_container: {
+        flex: 1,
+        margin: 16,
+        width: 174,
+        backgroundColor: "#FFFFFF",
+        paddingVertical: 20,
+        paddingHorizontal: 20,
+        shadowColor: '#B00020',
+        shadowOpacity: 0.70,
+        shadowRadius: 3,
+        shadowOffset: {
+            width: 0,
+            height: 4,
+        }
+    },
+    button_text: {
+        fontSize: 15,
+        color: "#B00020",
+        fontWeight: "bold",
+        alignSelf: "center",
+        textTransform: "capitalize"
+    },
 })
